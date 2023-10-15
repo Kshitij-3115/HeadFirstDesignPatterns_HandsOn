14a3304c965201b00c79043775866f70cf833b3c
Kshitij Khot <oneranks.nb@gmail.com>
2023-10-15 11:50:41 +0530

    First commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        // in this simple project we are going to demonstrate simple system design concepts.
+
+    }
+}
